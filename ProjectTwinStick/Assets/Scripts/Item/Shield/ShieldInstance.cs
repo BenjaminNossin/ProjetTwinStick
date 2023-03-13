f08e2025fad7dda36df46384ae12a9742bf3be99
PaperMartin <martin.wetischek@orange.fr>
2023-03-13 15:25:20 +0100

    stuff

--- ProjectTwinStick/Assets/Scripts/Item/Shield/ShieldInstance.cs
+++ ProjectTwinStick/Assets/Scripts/Item/Shield/ShieldInstance.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 
 public class ShieldInstance : MonoBehaviour, ILifeable
 {
-    
+    [SerializeField] GameObject shieldCollider;
+    private ShieldItemUpgrade _upgrade;
+    private float DisabledCooldown = 0f;
+
+    private bool isDisabled = false;
+    private bool isInUse = false;
     
     // Start is called before the first frame update
     void Start()
@@ -13,10 +18,55 @@ public class ShieldInstance : MonoBehaviour, ILifeable
         
     }
 
+    public void startUsing()
+    {
+        isInUse = true;
+        RefreshShieldState();
+    }
+
+    public void stopUsing()
+    {
+        isInUse = false;
+        RefreshShieldState();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if (isDisabled)
+        {
+            DisabledCooldown -= Time.deltaTime;
+            if(DisabledCooldown <= 0)
+            {
+                isDisabled = false;
+                shieldCollider.SetActive(true);
+                RefreshShieldState();
+            }
+        }
+    }
+
+    private void RefreshShieldState()
+    {
+        if (!isDisabled && isInUse)
+        {
+            shieldCollider.SetActive(true);
+        }
+        else
+        {
+            shieldCollider.SetActive(false);
+        }
+    }
+
+    private void DisableShield()
+    {
+        isDisabled = true;
+        shieldCollider.SetActive(false);
+        DisabledCooldown = _upgrade.shieldCooldown;
+    }
+
+    public void ChangeUpgrade(ShieldItemUpgrade upgrade)
+    {
+        _upgrade = upgrade;
     }
 
     public float GetMaxHp()
@@ -60,7 +110,7 @@ public class ShieldInstance : MonoBehaviour, ILifeable
     public event Action<float> OnIncreaseCurrentHp;
     public void DecreaseCurrentHp(float amount)
     {
-        throw new NotImplementedException();
+        DisableShield();
     }
 
     public event Action<float> OnDecreaseCurrentHp;
--- ProjectTwinStick/Assets/Scripts/Item/Shield/ShieldItem.cs
+++ ProjectTwinStick/Assets/Scripts/Item/Shield/ShieldItem.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShieldItem : Item
 {
     private ShieldItemSO _shieldItemSO;
     private ShieldItemUpgrade _currentUpgrade;
+    [SerializeField]
     private ShieldInstance _shieldInstance;
     
     public override ItemSO GetSO()
@@ -20,19 +22,34 @@ public class ShieldItem : Item
 
     private void OnItemStateChanged(ItemState state)
     {
-        if(state != ItemState.Thrown)
+        if(state != ItemState.Held)
         {
-            Debug.Log("Should disable shield");
+            DisableShield();
         }
+        else EnableShield();
+    }
+
+    private void DisableShield()
+    {
+        _shieldInstance.stopUsing();
+    }
+
+    private void EnableShield()
+    {
+        _shieldInstance.startUsing();
     }
 
     public override void Shoot(Vector3 startPosition, Vector2 direction)
     {
-        
+        //TODO : Raycast to edge of map instead
+        direction.Normalize();
+        _shieldInstance.transform.position = startPosition + new Vector3(direction.x, 0, direction.y) * 5f;
+        _shieldInstance.transform.forward = new Vector3(direction.x, 0, direction.y);
     }
 
     public override void SetUpgrade(ItemUpgrade newUpgrade)
     {
         _currentUpgrade = (ShieldItemUpgrade) newUpgrade;
+        _shieldInstance.ChangeUpgrade(_currentUpgrade);
     }
 }
