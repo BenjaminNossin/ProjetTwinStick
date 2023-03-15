ec65b8520cc0d7356355e063cd2f16d84feadcbe
Benji <f.nossin@rubika-edu.com>
2023-03-15 16:36:34 +0100

    timer and fixing flow bug (game over)

--- ProjectTwinStick/Assets/Scripts/GameManagement/GameStates/GameManager.cs
+++ ProjectTwinStick/Assets/Scripts/GameManagement/GameStates/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 using Game.Systems.AI;
 using UnityEngine.SceneManagement;
 using Game.Systems.GlobalFramework.States;
-using System.Reflection;
+using TMPro;
 
 namespace Game.Systems.GlobalFramework
 {
@@ -31,14 +31,17 @@ namespace Game.Systems.GlobalFramework
         [SerializeField] private GameObject optionsObj;
         [SerializeField] private GameObject gameOverObj;
         [SerializeField] private GameObject gameWonObj;
+        [SerializeField] private TMP_Text tmpTimer;
         #endregion
 
         #region Gameplay
         [Space, SerializeField, Range(1, 4)] private int playersRequiredAmount = 4;
         [Space, SerializeField, Range(1, 15)] private float minutesBeforeWin_Tutorial = 5;
         [Space, SerializeField, Range(1, 20)] private float minutesBeforeWin_MainGame = 12;
-        private bool isTutorial; 
-        private float minutesBeforeWin;
+        private bool isTutorial;
+        private bool playing; 
+        private float timeBeforeWin_AsSeconds;
+        private float currentPlayTime; 
 
         private int currentPlayerReadyCount;
         private readonly List<GameObject> waitRooms = new();
@@ -74,6 +77,25 @@ namespace Game.Systems.GlobalFramework
 
             currentContext = new(new LobbyState(), playerInputManager);
             Invoke(nameof(InitializeContext), 0.2f); 
+        }
+
+        float min, sec;
+        private void Update()
+        {
+            if (playing)
+            {
+                min = Mathf.FloorToInt(timeBeforeWin_AsSeconds / 60f); 
+                sec = timeBeforeWin_AsSeconds % 60f;
+
+                var span = new TimeSpan(0, 0, (int)timeBeforeWin_AsSeconds); //Or TimeSpan.FromSeconds(seconds); (see Jakob C´s answer)
+                var timeFormatted = string.Format("{0}:{1:00}",
+                                            (int)span.TotalMinutes,
+                                            span.Seconds);
+
+                tmpTimer.text = $"{timeFormatted}";
+
+                timeBeforeWin_AsSeconds -= Time.deltaTime;
+            }
         }
 
         public void InitializeContext()
@@ -186,6 +208,9 @@ namespace Game.Systems.GlobalFramework
 
         private void DeactivateAllGameplayObjects()
         {
+            Debug.Log("deactivating all gameplay objects"); 
+            playing = false; 
+
             SetObjectActive(ShipCoreObj, false);
             _gameEventTimelineReader.OnGameOver();
             waveManager.OnGameOver();
@@ -243,8 +268,10 @@ namespace Game.Systems.GlobalFramework
         public void OnGameStart()
         {
             Debug.Log("Starting Game. Is tutorial: " + isTutorial);
+            playing = true; 
 
-            Invoke(nameof(OnGameWin), (isTutorial ? minutesBeforeWin_Tutorial : minutesBeforeWin_MainGame) * 60f); // * 60f);
+            timeBeforeWin_AsSeconds = (isTutorial ? minutesBeforeWin_Tutorial : minutesBeforeWin_MainGame) * 60f; 
+            Invoke(nameof(OnGameWin), 30f); 
 
             SetAllUIIsActive(false);
 
@@ -263,6 +290,8 @@ namespace Game.Systems.GlobalFramework
 
         public void OnGameOver()
         {
+            CancelInvoke(nameof(OnGameWin));
+
             SetObjectActive(gameOverObj, true);
 
             DeactivateAllGameplayObjects();
@@ -285,6 +314,7 @@ namespace Game.Systems.GlobalFramework
 
         public void OnGameWin()
         {
+            Debug.Log("ON GAME WIN"); 
             SetObjectActive(gameWonObj, true);
 
             DeactivateAllGameplayObjects();
